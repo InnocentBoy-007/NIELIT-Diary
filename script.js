c1a28ace3cdb25a9a5a1fedb5d28071f975cf038
InnocentBoy <141760554+InnocentBoy-007@users.noreply.github.com>
2024-03-10 19:40:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+const btns = document.querySelectorAll('.nav-btn');
+const slides = document.querySelectorAll('.image-slide');
+const navItems = document.querySelectorAll('.navigation-items');
+const contents = document.querySelectorAll('.content');
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+})
+
+var sliderNav = function (manual) {
+    btns.forEach((btn) => {
+        btn.classList.remove("active")
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active")
+    });
+
+    contents.forEach((content) => {
+        content.classList.remove("active")
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
+
+sliderNav(0);
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        menuBtn.classList.remove("active");
+        navigation.classList.remove("active");
+    });
+});
+
+document.querySelectorAll('.pro img').forEach(img => {
+    img.addEventListener('click', function () {
+        const loading = document.querySelector('.loading');
+        loading.style.display = 'flex';
+
+        const newImg = new Image();
+        newImg.src = this.src;
+        newImg.onload = function () {
+            window.open(this.src, '_blank');
+            loading.style.display = 'none';
+        };
+    });
+});
+
+window.onload = function () {
+    document.getElementById('loading-overlay').style.display = 'none';
+}
+
+function customWelcome(message) {
+    Swal.fire({
+        title: 'Welcome to NIELIT Diary',
+        text: message,
+        icon: 'success',
+        confirmButtonText: 'Continue',
+        timer: 3000, // Close the welcome box after 5 seconds
+        showConfirmButton: true,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            setTimeout(() => {
+                Swal.close();
+            }, 3000);
+        },
+        customClass: {
+            title: 'custom-title',
+            container: 'custom-container',
+        },
+        width: '500',
+        heightAuto: false,
+    });
+}
+
+customWelcome('Explore your memories!');
